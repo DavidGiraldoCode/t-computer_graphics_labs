f199de23ce5fec4fe34129fbb40c63c6d8c9a192
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-03-20 20:52:36 +0100

    Abstracted methods to sutup and draw

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,16 @@
 #include <SDL2/SDL.h>
 #include <cmath>
 
+int status = 0;
 const int WIDTH = 1024;
 const int HEIGHT = 768;
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
 
 const std::vector<float> topLeftSample {255.0, 0.0, 0.0};
 const std::vector<float> topRigthtSample {0.0, 0.0, 255.0};
-const std::vector<float> bottomLeftSample {0.0, 255.0, 0.0};
-const std::vector<float> bottomRightSample {255.0, 255.0, 0.0};
+const std::vector<float> bottomLeftSample {0.0, 0.0, 255.0};
+const std::vector<float> bottomRightSample {255.0, 0.0, 0.0};
 
 std::vector<float> interpolatedSample (3,0.0); //3 sized vector with 0.0 in all positions
 
@@ -46,53 +49,44 @@ void bilinearInterpolation(
         topRowInterpolation[i] = (((maxPoint[0] - point[0])/maxPoint[0]) * topLeftSample[i] ) + ((point[0]/maxPoint[0]) * topRigthtSample[i]);
         bottomRowInterpolation[i] = (((maxPoint[0] - point[0])/maxPoint[0]) * bottomLeftSample[i] ) + ((point[0]/maxPoint[0]) * bottomRightSample[i]);
         interpolatedSample[i]  = (((maxPoint[1] - point[1])/maxPoint[1]) * topRowInterpolation[i] ) + ((point[1]/maxPoint[1]) * bottomRowInterpolation[i]);
-
     }
 }
 
-
-int main(int argc, char * argv[])
+void setup()
 {
-    std::cout << "Hello Window! \n";
-
-    //linearInterpolation(nx/4, nx, topLeftSample, topRigthtSample, interpolatedSample);
-    //bilinearInterpolation( point,  maxPoint, topLeftSample, topRigthtSample, bottomLeftSample, bottomRightSample, interpolatedSample);
-
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
         std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
-        return -1;
+        status = -1;
     }
-    else
-    {
-        std::cout << "SDL_Init succeeded \n";
-    }
+    std::cout << "SDL_Init succeeded \n";
+}
 
-    SDL_Window* window = SDL_CreateWindow("Video Game - SDL window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+void createWindowRenderer(const int WIDTH, const int HEIGHT, SDL_Window* & window, SDL_Renderer* & renderer )
+{
+    window = SDL_CreateWindow("Video Game - SDL window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if (!window)
     {
         std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
         SDL_Quit();
-        return -1;
+        status = -1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer)
     {
         std::cerr << "SDL_CreateRenderer failed: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
-        return -1;
+        status = -1;
     }
+}
 
-    // Set the background color to black
-    //SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+void draw(SDL_Renderer* & renderer)
+{
     SDL_RenderClear(renderer);
-
-    // Render a single white pixel in the middle
     for(size_t i = 0; i < (WIDTH*HEIGHT) - 1; i++)
     {   
-        
         point[0] = i % int(nx);
         point[1] = int(floor( int(i) / int(nx))) % int(ny);
         bilinearInterpolation( point,  maxPoint, topLeftSample, topRigthtSample, bottomLeftSample, bottomRightSample, interpolatedSample);
@@ -100,11 +94,11 @@ int main(int argc, char * argv[])
         SDL_SetRenderDrawColor(renderer, interpolatedSample[0], interpolatedSample[1], interpolatedSample[2], 255);
         SDL_RenderDrawPoint(renderer, point[0], point[1]);
     }
-    
-    //SDL_Point();
-    //SDL_RenderDrawPoints();
     SDL_RenderPresent(renderer);
+}
 
+void events()
+{
     bool isRunning = true;
     SDL_Event event;
     while (isRunning)
@@ -117,12 +111,24 @@ int main(int argc, char * argv[])
             }
         }
     }
+}
+
+int main(int argc, char * argv[])
+{
+    std::cout << "Hello Window! \n";
+    setup();
+    createWindowRenderer(WIDTH, HEIGHT, window, renderer);
+    if(status != 0)
+    {
+        return -1;
+    }
+    draw(renderer);
+    events();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
     return 0;
-
 }
 
