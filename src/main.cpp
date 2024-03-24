d7b4a750350673e2337f6f739cb7871a00d1f93f
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-03-24 11:01:39 +0100

    Cleaned code

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,10 @@ const int HEIGHT = 768;
 SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 
-const std::vector<float> topLeftSample {255.0, 0.0, 0.0};
-const std::vector<float> topRigthtSample {0.0, 0.0, 255.0};
-const std::vector<float> bottomLeftSample {0.0, 0.0, 255.0};
-const std::vector<float> bottomRightSample {255.0, 0.0, 0.0};
-
-
+const std::vector<float> topLeftSample {255.0, 255.0, 255.0};
+const std::vector<float> topRigthtSample {180.0, 255.0, 250.0};
+const std::vector<float> bottomLeftSample {200.0, 255.0, 200.0};
+const std::vector<float> bottomRightSample {255.0, 255.0, 255.0};
 
 std::vector<float> interpolatedSample (3,0.0); //3 sized vector with 0.0 in all positions
 
@@ -28,7 +26,7 @@ const float ny = HEIGHT+0.0f;
 float point[3] = {512.0, 512.0, 512};
 const float maxPoint[2] = {nx, ny};
 
-void interpolation(int initialSample, int finalSample, std::vector<float>& result)
+void interpolation(const int initialSample, const int finalSample, std::vector<float>& result)
 {
     //Inerpolation in which "t" blongs to [0,1]
     if(result.size() > 2)
@@ -38,15 +36,35 @@ void interpolation(int initialSample, int finalSample, std::vector<float>& resul
         {
             std::cout << "t: "<< t <<"\n";
             result[i] = ((1 - t) * initialSample) + (t * finalSample);
-            t +=  1.0 / (result.size()-1);
-            
+            t +=  1.0 / (result.size()-1);      
         }   
         
     } else
     {
         std::cout << "Result vector must be greater than 2\n";
     }
-    
+}
+
+void vectorInterpolation(const std::vector<float>& initialSample, const std::vector<float>& finalSample, std::vector<std::vector<float>>& result)
+{
+    //Inerpolation in which "t" blongs to [0,1]
+    if(result.size() > 2)
+    {
+        float t = 0;
+        for(size_t j = 0; j < result.size(); j++)
+        {
+            result[j] = {0,0,0};
+            for(size_t i = 0; i < result[j].size(); i++)
+            {
+                result[j][i] = ((1 - t) * initialSample[i]) + (t * finalSample[i]);
+            }
+            t +=  1.0 / (result.size()-1);      
+        }   
+        
+    } else
+    {
+        std::cout << "Result vector must be greater than 2\n";
+    }
 }
 
 void linearInterpolation(int x, float domain, 
@@ -121,16 +139,29 @@ void draw(SDL_Renderer* & renderer)
     {
         std::cout << result[i] << "\n";
     }
+ 
+    const int INTERPOLATED_VECTORS = 4;
+    std::vector<std::vector<float>> resultVector(INTERPOLATED_VECTORS);
+    std::vector<float> startVector = {1, 4, 9.2};
+    std::vector<float> endVector = {4, 1, 9.8};
+
+    vectorInterpolation(startVector, endVector, resultVector);
+    for(size_t i = 0; i < resultVector.size(); i++)
+    {
+        std::cout << "( "<< resultVector[i][0] << ", ";
+        std::cout << resultVector[i][1] << ", ";
+        std::cout << resultVector[i][2] << ") \n";
+    }
 
     SDL_RenderClear(renderer);
     for(size_t i = 0; i < (WIDTH*HEIGHT) - 1; i++)
     {   
-        point[0] = i % int(nx);
-        point[1] = int(floor( int(i) / int(nx))) % int(ny);
+        point[0] = i % int(nx); // Linear index mapped to x-coodinate system
+        point[1] = int(floor( int(i) / int(nx))) % int(ny); // Linear index mapped to y-coodinate system
         bilinearInterpolation( point,  maxPoint, topLeftSample, topRigthtSample, bottomLeftSample, bottomRightSample, interpolatedSample);
         
-        //SDL_SetRenderDrawColor(renderer, interpolatedSample[0], interpolatedSample[1], interpolatedSample[2], 255);
-        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        SDL_SetRenderDrawColor(renderer, interpolatedSample[0], interpolatedSample[1], interpolatedSample[2], 255);
+        //SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
         SDL_RenderDrawPoint(renderer, point[0], point[1]);
     }
     SDL_RenderPresent(renderer);
